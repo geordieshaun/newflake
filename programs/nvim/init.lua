70cb6cf6e2ffaa7b1d7c633e7f4c116cff5e3a1d
Shaun Clough <shaun.clough@googlemail.com>
2023-03-23 19:59:49 +0000

    another attempt at fixing treesiter rw issue!

--- programs/nvim/init.lua
+++ programs/nvim/init.lua
@@ -48,9 +48,16 @@ opt.incsearch = true
 opt.hidden = true
 opt.shortmess = "atI"
 
+-- create rw dir for treesitters in nvim's cache dir
+local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+vim.fn.mkdir(parser_install_dir, "p")
+-- Prevents reinstall of treesitter plugins every boot
+vim.opt.runtimepath:append(parser_install_dir)
+
 require'nvim-treesitter.configs'.setup {
     ensure_installed = { "all" },
     highlight = {
         enable = true,
     },
+    parser_install_dir = parser_install_dir
 }
